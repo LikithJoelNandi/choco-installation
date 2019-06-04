c0a9979d42025c656088d76fd8d642e9695504f6
LikithJoelNandi <49515456+LikithJoelNandi@users.noreply.github.com>
2019-06-04 14:48:58 +0530

    Create install_cloudnativelaptop_softwares.ps1

---
+++ install_cloudnativelaptop_softwares.ps1
@@ -0,0 +1,20 @@
+# Upgrae chocolatey
+choco upgrade chocolatey
+
+# Install Google Chrome
+choco install -y googlechrome
+
+# Install 7-Zip
+choco install -y 7zip
+
+# Install Git
+choco install -y git
+
+# Install Putty
+choco install -y putty
+
+# Install WinSCP
+choco install -y winscp
+
+# Install Notepad++
+choco install -y notepadplusplus
